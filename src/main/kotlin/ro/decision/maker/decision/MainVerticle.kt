99be36fe9f43e44ae684b76de69265eac0d8bc40
Johann Fazakas <johannfazakas@gmail.com>
2020-10-17 17:48:38 +0300

    initial commit. simple vertx app with http server.

---
+++ src/main/kotlin/ro/decision/maker/decision/MainVerticle.kt
@@ -0,0 +1,25 @@
+package ro.decision.maker.decision
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Promise
+
+class MainVerticle : AbstractVerticle() {
+
+  override fun start(startPromise: Promise<Void>) {
+    vertx
+      .createHttpServer()
+      .requestHandler { req ->
+        req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!")
+      }
+      .listen(9020) { http ->
+        if (http.succeeded()) {
+          startPromise.complete()
+          println("HTTP server started on port 8888")
+        } else {
+          startPromise.fail(http.cause());
+        }
+      }
+  }
+}
